a3ef10ab4df526f623ed982e5829bf986f2cf2d4
Jade1724 <haruka.i1724@gmail.com>
2023-02-24 17:09:00 +1300

    Creted task validator and used in the tasks router.

--- src/tasks/tasks.router.ts
+++ src/tasks/tasks.router.ts
@@ -1,5 +1,7 @@
 import { Router, Request, Response } from 'express';
 import { TasksController } from './tasks.controller';
+import { createValidator } from './tasks.validator';
+import { validationResult } from 'express-validator/src/validation-result';
 
 // Fire the router function
 export const tasksRouter: Router = Router();
@@ -11,5 +13,18 @@ tasksRouter.get(
     const taskController = new TasksController();
     const allTasks = await taskController.getAll();
     res.json(allTasks).status(200);
+  },
+);
+
+tasksRouter.post(
+  '/tasks',
+  createValidator,
+  //@ts-ignore
+  async (req: Request, res: Response) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+        return res.status(400).json({errors: errors.array()});
+    }
   },
 );
